0cad7c5481b4fd732dcf88a1a7fdc4c3db78ab65
tom <tom@ohhhh.me>
2017-07-12 18:07:37 -0700

    made release section of home page

--- _site/js/scrolling-home.js
+++ _site/js/scrolling-home.js
@@ -3,11 +3,26 @@ $(window).scroll(function () {
     var wScroll = $this.scrollTop();
 
     parallaxNavbar(wScroll, $this);
+    parallaxTour(wScroll, $this);
 
 });
 
 function parallaxNavbar (wScroll, $this) {
-    if (wScroll > $this.height() * 0.8) {
-        $('.sidebar').addClass('is-visible');
+    if (wScroll > $this.height() * 0.65) {
+        $('.logo').addClass('is-visible');
+        setTimeout(function () {
+            $('.sidebar').addClass('is-visible');
+        }, 500);
     }
 }
+
+
+function parallaxTour (wScroll, $this) {
+    var offset = $('.home__tour').offset().top;
+    var opacity = Math.min((offset - wScroll - $this.height() / 4) / (- $this.height() / 4), 1);
+
+    $('.home__tour__heading').css({
+        'opacity': opacity
+    });
+}
+
