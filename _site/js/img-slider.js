6a3c893ac76409ad0994cd06fb118e403ae6e5e7
tom <tom@ohhhh.me>
2017-07-16 16:14:21 -0700

    finished gallery page

--- _site/js/img-slider.js
+++ _site/js/img-slider.js
@@ -1,25 +1,27 @@
 /*
-* IMAGE SLIDER
-* */
+ * IMAGE SLIDER
+ * */
 
-var imgs = document.getElementsByClassName('img-slider__img');
+var imgs = document.getElementsByClassName('gallery__slider__img');
+var caption = document.getElementsByClassName('gallery__caption')[0];
 var index = 0;
 
 // set up initial opacity for the images
-for (var i = 1; i < imgs.length; i++) {
-    imgs[i].style.opacity = 0;
-}
+resetStyles();
+
+// set up initial active thumb
+showActiveImage(index);
 
 function imgSlider(inc) {
-
+    // handle event when control arrows are clicked
     // find out the index of the image
-    if (inc = 1) {
+    if (inc === 1) {
         if (index === imgs.length - 1) {
             index = 0;
         } else {
             index++;
         }
-    } else if (inc = -1) {
+    } else if (inc === -1) {
         if (index === 0) {
             index = imgs.length - 1;
         } else {
@@ -27,11 +29,23 @@ function imgSlider(inc) {
         }
     }
 
-    // change opacity for all images to 0
+    resetStyles();
+    showActiveImage(index);
+
+}
+
+
+// HELPERS
+
+function resetStyles() {
+    // change opacity for all images to 0 and change style of thumbs
     for (var i = 0; i < imgs.length; i++) {
         imgs[i].style.opacity = 0;
     }
+}
 
-    // change opacity for active image to 1
+function showActiveImage(index) {
+    // change opacity for active image to 1 and change style of thumbs
     imgs[index].style.opacity = 1;
+    caption.innerText = imgs[index].alt;
 }
